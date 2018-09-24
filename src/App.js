c775ba00b4b1a71959ab112f08bbf7e36a6a5b6e
ziagrosvenor <ziagrosvenor@live.co.uk>
2018-09-24 16:42:37 +0100

    Use airport data + crossfilter to update barchart component

--- src/App.js
+++ src/App.js
@@ -2,13 +2,50 @@ import React, {Component} from 'react';
 import '../node_modules/material-components-web/dist/material-components-web.min.css';
 import styled from 'styled-components';
 import {Grid, GridCell, GridInner} from '@rmwc/grid';
+import crossfilter from 'crossfilter2';
+import {csvParse} from 'd3-dsv';
 
 import {TopAppBar, DonutChart, BarChart} from './components';
 
 const RootContainer = styled.div``;
 const ContentContainer = styled.div``;
 
+function parseDate(d) {
+  return new Date(
+    2001,
+    d.substring(0, 2) - 1,
+    d.substring(2, 4),
+    d.substring(4, 6),
+    d.substring(6, 8),
+  );
+}
+
 class App extends Component {
+  state = {
+    hours: null,
+    loading: true,
+  };
+  async componentDidMount() {
+    this.setState({loading: true});
+    const response = await fetch('./flights.csv');
+    const csvData = await response.text();
+    const flights = csvParse(csvData, (d, i) => {
+      d.index = i;
+      d.date = parseDate(d.date);
+      d.delay = +d.delay;
+      d.distance = +d.distance;
+      return d;
+    });
+
+    const flightsFilter = crossfilter(flights);
+    const hour = flightsFilter.dimension(
+      d => d.date.getHours() + d.date.getMinutes() / 60,
+    );
+    this.setState({
+      loading: false,
+      hours: hour.group(Math.floor),
+    });
+  }
   render() {
     return (
       <RootContainer>
@@ -19,7 +56,11 @@ class App extends Component {
               <DonutChart />
             </GridCell>
             <GridCell span="6">
-              <BarChart />
+              {this.state.loading ? (
+                undefined
+              ) : (
+                <BarChart domain={[0, 24]} data={this.state.hours} />
+              )}
             </GridCell>
           </Grid>
         </ContentContainer>
--- src/components/molecules/BarChart/BarChart.js
+++ src/components/molecules/BarChart/BarChart.js
@@ -1,6 +1,5 @@
 // @flow weak
 /* eslint react/no-multi-comp: 'off' */
-// example from https://bl.ocks.org/mbostock/3885705
 
 import React, {PureComponent} from 'react';
 import NodeGroup from 'react-move/NodeGroup';
@@ -21,43 +20,7 @@ const dims = [
   view[1] - trbl[0] - trbl[2],
 ];
 
-// **************************************************
-//  Mock Data
-// **************************************************
-const letters = [
-  {letter: 'A', frequency: 0.08167},
-  {letter: 'B', frequency: 0.01492},
-  {letter: 'C', frequency: 0.0278},
-  {letter: 'D', frequency: 0.04253},
-  {letter: 'E', frequency: 0.12702},
-  {letter: 'F', frequency: 0.02288},
-  {letter: 'G', frequency: 0.02022},
-  {letter: 'H', frequency: 0.06094},
-  {letter: 'I', frequency: 0.06973},
-  {letter: 'J', frequency: 0.00153},
-  {letter: 'K', frequency: 0.00747},
-  {letter: 'L', frequency: 0.04025},
-  {letter: 'M', frequency: 0.02517},
-  {letter: 'N', frequency: 0.06749},
-  {letter: 'O', frequency: 0.07507},
-  {letter: 'P', frequency: 0.01929},
-  {letter: 'Q', frequency: 0.00098},
-  {letter: 'R', frequency: 0.05987},
-  {letter: 'S', frequency: 0.06333},
-  {letter: 'T', frequency: 0.09056},
-  {letter: 'U', frequency: 0.02758},
-  {letter: 'V', frequency: 0.01037},
-  {letter: 'W', frequency: 0.02465},
-  {letter: 'X', frequency: 0.0015},
-  {letter: 'Y', frequency: 0.01971},
-  {letter: 'Z', frequency: 0.00074},
-];
-
-const y = scaleLinear()
-  .range([dims[1], 0])
-  .domain([0, max(letters, d => d.frequency)]);
-
-class Example extends PureComponent {
+export class BarChart extends PureComponent {
   state = {
     sortAlpha: true,
   };
@@ -68,62 +31,56 @@ class Example extends PureComponent {
     }));
   };
 
+  componentDidMount() {}
+
   render() {
     const {sortAlpha} = this.state;
-
-    const sorted = letters
-      .sort(
-        sortAlpha
-          ? (a, b) => ascending(a.letter, b.letter)
-          : (a, b) => b.frequency - a.frequency,
-      )
-      .slice(0);
+    const y = scaleLinear()
+      .range([dims[1], 0])
+      .domain([0, max(this.props.data.all(), d => d.value)]);
 
     const scale = scaleBand()
       .rangeRound([0, dims[0]])
-      .domain(sorted.map(d => d.letter))
+      .domain(this.props.data.all().map(d => d.key))
       .padding(0.1);
 
     return (
       <div>
-        <button onClick={this.update}>
-          {`Sort ${sortAlpha ? 'Value' : 'Alpha'}`}
-        </button>
         <Surface view={view} trbl={trbl}>
           <NodeGroup
-            data={sorted}
-            keyAccessor={d => d.letter}
+            data={this.props.data.all()}
+            keyAccessor={d => d.key}
             start={() => ({
               opacity: 1e-6,
-              x: 1e-6,
+              y: 1e-6,
               width: scale.bandwidth(),
             })}
             enter={d => ({
               opacity: [0.7],
-              x: [scale(d.letter)],
+              y: [y(d.value)],
               timing: {duration: 750, ease: easeExpInOut},
             })}
             update={(d, i) => ({
               opacity: [0.7],
-              x: [scale(d.letter)],
+              y: [y(d.value)],
               width: [scale.bandwidth()],
               timing: {duration: 750, delay: i * 50, ease: easeExpInOut},
             })}
             leave={() => ({
               opacity: [1e-6],
-              x: [scale.range()[1]],
+              y: [y.range()[1]],
               timing: {duration: 750, ease: easeExpInOut},
             })}>
             {nodes => (
               <g>
                 {nodes.map(({key, data, state}) => {
-                  const {x, ...rest} = state;
+                  const {y, ...rest} = state;
 
                   return (
-                    <g key={key} transform={`translate(${x},0)`}>
+                    <g key={key} transform={`translate(${scale(data.key)},0)`}>
                       <rect
-                        height={dims[1] - y(data.frequency)}
-                        y={y(data.frequency)}
+                        height={dims[1] - y}
+                        y={y}
                         fill="#00a7d8"
                         {...rest}
                       />
@@ -132,7 +89,7 @@ class Example extends PureComponent {
                         y={dims[1] + 15}
                         dx="-.35em"
                         fill="#333">
-                        {data.letter}
+                        {data.key}
                       </text>
                     </g>
                   );
@@ -145,5 +102,3 @@ class Example extends PureComponent {
     );
   }
 }
-
-export const BarChart = Example;
